76b196a2d0b7b7585947f068d985d15520e88267
Frank2446-dotcom <150971995+Frank2446-dotcom@users.noreply.github.com>
2024-03-19 14:11:41 +0300

    Add files via upload

---
+++ Hackathon_1_dart_&_flutter/program_1.dart
@@ -0,0 +1,22 @@
+void main() {
+  String name = " Frankline Alele Ambetsa";
+  int age = 26;
+  String school = "Koyonzo Boys High School";
+  String polytechnic ="The Eldoret National Polytechnic.";
+  
+  String course1 = "Diploma in Civil Engineering";
+  String academy = "Power Learn Project (PLP)";
+  String course2 = "Software Engineering.";
+  String university = "University of the People";
+  String course3 = "Bachelor Degree in Computer Science";
+  String institute = "Alison";
+  String course4 = "Data Science";
+  String google = "Google Analytics";
+  String FutureCourse = "Rootics, Machine Learning and Artificial Intelligence";
+  
+  String hobby = "Coding";
+  
+  print("My name is $name. I am $age years old, and I attend $school. I studied $course1 at $polytechnic Currently I am studying at $academy taking $course2 I am also studying $course3 at The $university. My hobby is $hobby.");
+  print("Moreover, I am  studying $course4 at $institute and $google."); // additional information for my personal knowledge development
+  print("In future, I want to study $FutureCourse."); //additional information for my personal knowledge improvement
+}
---
+++ Hackathon_1_dart_&_flutter/program_2.dart
@@ -0,0 +1,15 @@
+void main() {
+  int a = 16;
+  int b = 55;
+  
+  print("Addition result: ${add(a, b)}");
+  print("Subtraction result: ${subtract(a, b)}");
+}
+
+int add(int x, int y) {
+  return x + y;
+}
+
+int subtract(int x, int y) {
+  return x - y;
+}
